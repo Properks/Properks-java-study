71888850b9c3a04272402be8fa6096850edd29e4
Jeongmo <wjdah1402@gmail.com>
2023-03-12 13:40:08 +0900

    Char type variable

--- Chapter_02/Variable_and_Datatype.java
+++ Chapter_02/Variable_and_Datatype.java
@@ -6,8 +6,16 @@ package Chapter_02;
 public class Variable_and_Datatype {
 
     public static void main(String[] args) {
-        int level; // Declare variable
-        level = 10;
-        System.out.println(level); // print variable
+        char ch1 = 'A';
+        System.out.println(ch1);
+        System.out.println((int)ch1);
+
+        char ch2 = 66;
+        System.out.println(ch2);
+
+        int ch3 = 'C';
+        System.out.println(ch3);
+        System.out.println((char)ch3);
+
     }
 }

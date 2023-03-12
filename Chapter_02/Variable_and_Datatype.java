7dd4facf75fbe057e931ec914cb24b2f244e50b6
Jeongmo <wjdah1402@gmail.com>
2023-03-12 13:29:37 +0900

    Declare variable and print

--- Chapter_02/Variable_and_Datatype.java
+++ Chapter_02/Variable_and_Datatype.java
@@ -6,6 +6,8 @@ package Chapter_02;
 public class Variable_and_Datatype {
 
     public static void main(String[] args) {
-        
+        int level; // Declare variable
+        level = 10;
+        System.out.println(level); // print variable
     }
 }

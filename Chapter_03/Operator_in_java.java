30872c75daa305235dfaea79ac37108de3c7eab9
Jeongmo <wjdah1402@gmail.com>
2023-03-14 10:53:20 +0900

    Basic operator in java

--- Chapter_03/Operator_in_java.java
+++ Chapter_03/Operator_in_java.java
@@ -6,6 +6,11 @@ package Chapter_03;
 public class Operator_in_java {
 
     public static void main(String[] args) {
-        
+        int mathscore = 92;
+        int engscore = 87;
+
+        int totalscore = mathscore + engscore;
+        System.out.println("Average : ");
+        System.out.println(totalscore/2);
     }
 }

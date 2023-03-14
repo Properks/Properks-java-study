53ac6b1d74018bc37e43f7789b9b0dd1dbfb3768
Jeongmo <wjdah1402@gmail.com>
2023-03-14 11:03:40 +0900

    Logical operator and Conditional operator #3

--- Chapter_03/Operator_in_java.java
+++ Chapter_03/Operator_in_java.java
@@ -8,9 +8,16 @@ public class Operator_in_java {
     public static void main(String[] args) {
         int mathscore = 92;
         int engscore = 87;
+        boolean value;
+        String str;
 
         int totalscore = mathscore + engscore;
-        System.out.println("Average : ");
+        System.out.print("Average : ");
         System.out.println(totalscore/2);
+
+        value = (mathscore > 90)||(engscore > 90);
+        System.out.println(value);
+        str = (value) ? "Reach your goal" : "Sorry, you don't reach a goal";
+        System.out.println(str);
     }
 }

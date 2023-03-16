392817e026d1e53f4ff152912cc310c6f9e6c9e4
Jeongmo <wjdah1402@gmail.com>
2023-03-16 15:42:40 +0900

    Search prime number among numbers that are under 100

--- Chapter_04/Conditional_and_loop.java
+++ Chapter_04/Conditional_and_loop.java
@@ -6,13 +6,28 @@ package Chapter_04;
 public class Conditional_and_loop {
 
     public static void main(String[] args) {
-        int sum = 1, num = 2;
-
-        while(sum % 50 != 0)
-        {
-            sum += num;
-            num++;
+        for (int i = 1; i < 100; i++) {
+            int count = 0;
+            if(i == 1 || i ==2 || i == 3)
+            {
+                System.out.print(i);
+                System.out.println("is prime number");
+            }
+            else
+            {
+                for (int j = 2; j <= i/2; j++) {
+                    if (i % j == 0) {
+                        System.out.print(i);
+                        System.out.println("isn't prime number");
+                        count++;
+                        break;
+                    }
+                }
+                if (count == 0) {
+                    System.out.print(i);
+                    System.out.println("is prime number");
+                }
+            }           
         }
-        System.out.println(sum);
     }
 }

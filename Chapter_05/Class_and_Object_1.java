05128de76ff020514a6ddfe1c81833a649b28092
Jeongmo <wjdah1402@gmail.com>
2023-03-17 15:02:35 +0900

    Practice function

--- Chapter_05/Class_and_Object_1.java
+++ Chapter_05/Class_and_Object_1.java
@@ -1,11 +1,18 @@
 package Chapter_05;
 
 /**
- * Class_and_Object-1
+ * Class_and_Object_1
  */
 public class Class_and_Object_1 {
 
     public static void main(String[] args) {
-        
+        int number = 5;
+        int expo = 0;
+        expo = exponent(number);
+        System.out.println(expo);
+    }
+
+    public static int exponent(int num) {
+        return (num * num);
     }
 }

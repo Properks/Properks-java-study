fbf2d2120fecf3c869f1d56a7e40818b9edc2557
Jeongmo <wjdah1402@gmail.com>
2023-03-19 16:31:06 +0900

    System that calculate fee when taking a bus

--- Chapter_06/Class_and_Object_2.java
+++ Chapter_06/Class_and_Object_2.java
@@ -6,10 +6,12 @@ package Chapter_06;
 public class Class_and_Object_2 {
 
     public static void main(String[] args) {
-        Person noName = new Person();
-        Person Amy = new Person("Amy", 21);
+        Person noName = new Person("John", 17, 8000);
+        Person Amy = new Person("Amy", 21, 12000);
 
-        noName.printData();
-        Amy.printData();
+        noName.takeBus();
+        noName.showInfo();
+        Amy.takeBus();
+        Amy.showInfo();
     }
 }
--- Chapter_06/Person.java
+++ Chapter_06/Person.java
@@ -7,18 +7,47 @@ public class Person {
 
     String name;
     int age;
+    int money;
 
     public Person() {
-        this("Don't exist", 0);
+        this("Don't exist", 0, 0);
     }
 
-    public Person(String name, int age) {
+    public Person(String name, int age, int money) {
         this.name = name;
         this.age = age;
+        this.money = money;
     }
 
-    public void printData() {
-        System.out.println("Name : " + this.name);
-        System.out.println("Age : " + this.age);
+    public void takeBus() {
+        if(this.age < 19 && this.age >= 12)
+        {
+            checkMoney(this, 1100);
+        }
+        else if(this.age < 11)
+        {
+            checkMoney(this, 800);
+        }
+        else
+        {
+            checkMoney(this, 1500);
+        }
+    }
+
+    public void checkMoney(Person name, int money) {
+        if(name.money >= money)
+        {
+            name.money -= money;
+        }
+        else
+        {
+            System.out.println(this.name + "!! Not enough money");
+        }
+    }
+
+    public void showInfo() {
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("Money : " + money);
     }
 }

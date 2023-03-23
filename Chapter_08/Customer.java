3bd67ca348c483feff89240cc320f373677cd4d7
Jeongmo <wjdah1402@gmail.com>
2023-03-23 15:45:52 +0900

    Add comment and fix a little in customer's constructor

--- Chapter_08/Customer.java
+++ Chapter_08/Customer.java
@@ -21,32 +21,37 @@ public class Customer {
     }
 
     public Customer(int ID, String name) {
-        this.customerID = ID;
+        this.customerID = ID;  // TODO: manage ID in main function
         this.customerName = name;
-        this.totalBought = 0;
+        this.totalBought = 0; // TODO: totalbought = price - point, If use point
         this.point = 0;
         setGrade();
-        setPoint(0);
     }
 
-    public void setGrade() {
-        if (this.totalBought <= 2000) {
-            this.customerGrade = "Rookie";
+    public void setGrade() { 
+        if (this.totalBought < 100000) {
+            this.customerGrade = "Rookie"; // 0 ~ 99,999
+            this.bonusRatio = 0.0;
         }
-        else if (this.totalBought < 10000) {
+        else if (this.totalBought < 200000) { // 100,000 ~ 199,999
             this.customerGrade = "Silver";
+            this.bonusRatio = 0.02;
         }
-        else if (this.totalBought < 100000) {
+        else if (this.totalBought < 500000) { // 200,000 ~ 499,999
             this.customerGrade = "Gold";
+            this.bonusRatio = 0.03;
         }
-        else if (this.totalBought < 200000) {
+        else if (this.totalBought < 1000000) { // 500,000 ~ 999,999
             this.customerGrade = "Diamond";
+            this.bonusRatio = 0.04;
         }
-        else if (this.totalBought < 500000) {
+        else if (this.totalBought < 2000000) { // 1,000,000 ~ 2,000,000
             this.customerGrade = "VIP";
+            this.bonusRatio = 0.07;
         }
-        else if (this.totalBought < 1000000) {
-            this.customerGrade = "VIP";
+        else if (this.totalBought >= 2000000) { // 2,000,000 ~
+            this.customerGrade = "VVIP";
+            this.bonusRatio = 0.1;
         }
     }
 

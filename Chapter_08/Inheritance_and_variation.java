d785c8a2301f5070b3bcf5ca118eb732e2e91053
Jeongmo <wjdah1402@gmail.com>
2023-03-25 14:00:37 +0900

    Print Customer list and add TODO comment

--- Chapter_08/Inheritance_and_variation.java
+++ Chapter_08/Inheritance_and_variation.java
@@ -2,16 +2,21 @@ package Chapter_08;
 
 import java.util.ArrayList;
 
+
 /**
  * Inheritance_and_variation
  */
 public class Inheritance_and_variation {
 
     public static void main(String[] args) {
+        ArrayList<Customer> listCustomer = new ArrayList<Customer>();
         ArrayList<VVIPcustomer> listVVIP = new ArrayList<VVIPcustomer>(); // list of VVIP
 
         Customer John = new Customer(202310001, "John"); // Declare customer
         Customer Amy = new Customer(202310002, "Amy");
+
+        listCustomer.add(John);
+        listCustomer.add(Amy);
 
         System.out.println("=======Amy=======");
         Amy = buyProcess(listVVIP, Amy, 130000);
@@ -27,11 +32,9 @@ public class Inheritance_and_variation {
         John = buyProcess(listVVIP, John, 1000000); //buySomething in buyProcess is VVIPcustomer's method
         John.showInfo();
         System.out.println(John.getTotal()); // Show whether sale applies to Customer John or not
-
-        System.out.println("=====VVIP list====="); // Print VVIP list
-        for (int i = 0; i < listVVIP.size(); i++) {
-            System.out.println(listVVIP.get(i).getAgentID() + " : " + listVVIP.get(i).customerName);
-        }
+    
+        System.out.println("=====Customer list====="); // Print VVIP list
+        printList(listCustomer);
     }
 
     public static Customer buyProcess(ArrayList<VVIPcustomer> listVVIP, Customer name, int price) {
@@ -48,6 +51,13 @@ public class Inheritance_and_variation {
         }
         else{
             return name;
+        }
+    }
+    // TODO: Fucntion that sort ArrayList
+
+    public static void printList(ArrayList<Customer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i + 1) + list.get(i).customerName + "(" + list.get(i).getGrade() + ")" + " : " + list.get(i).totalBought);
         }
     }
 }

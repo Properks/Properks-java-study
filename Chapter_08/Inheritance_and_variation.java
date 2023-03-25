8df1e1fc7587dbf4d06983eb388bc52c988ae4b0
Jeongmo <wjdah1402@gmail.com>
2023-03-25 14:46:02 +0900

    Fix error that listcutomer is no longer refered object, when redeclare as VVIPcustomer()

--- Chapter_08/Inheritance_and_variation.java
+++ Chapter_08/Inheritance_and_variation.java
@@ -7,46 +7,54 @@ import java.util.ArrayList;
  * Inheritance_and_variation
  */
 public class Inheritance_and_variation {
-
+    
+    private static ArrayList<Customer> listCustomer = new ArrayList<Customer>();
+    private static ArrayList<VVIPcustomer> listVVIP = new ArrayList<VVIPcustomer>(); // list of VVIP
     public static void main(String[] args) {
-        ArrayList<Customer> listCustomer = new ArrayList<Customer>();
-        ArrayList<VVIPcustomer> listVVIP = new ArrayList<VVIPcustomer>(); // list of VVIP
-
         Customer John = new Customer(202310001, "John"); // Declare customer
         Customer Amy = new Customer(202310002, "Amy");
+        Customer Jack = new Customer(202310003, "Jack");
 
-        listCustomer.add(John);
+
+        listCustomer.add(John); // TODO: When new customer come in, add in 'listcustomer' automatically
         listCustomer.add(Amy);
+        listCustomer.add(Jack);
 
         System.out.println("=======Amy=======");
-        Amy = buyProcess(listVVIP, Amy, 130000);
+        Amy = buyProcess(Amy, 130000);
         Amy.showInfo();
 
-        Amy = buyProcess(listVVIP, Amy, 500000);
+        Amy = buyProcess(Amy, 500000);
         Amy.showInfo();
 
         System.out.println("=======John=======");
-        John = buyProcess(listVVIP, John, 2560000); // In order to show VVIP
+        John = buyProcess(John, 2560000); // In order to show VVIP
         John.showInfo();
 
-        John = buyProcess(listVVIP, John, 1000000); //buySomething in buyProcess is VVIPcustomer's method
+        John = buyProcess(John, 1000000); //buySomething in buyProcess is VVIPcustomer's method
         John.showInfo();
+
         System.out.println(John.getTotal()); // Show whether sale applies to Customer John or not
+
+        System.out.println("=======Jack=======");
+        Jack = buyProcess(Jack, 3200000);
+        Jack = buyProcess(Jack, 1000000);
+        Jack.showInfo();
+        System.out.println(Jack.getTotal());
     
         System.out.println("=====Customer list====="); // Print VVIP list
         printList(listCustomer);
     }
 
-    public static Customer buyProcess(ArrayList<VVIPcustomer> listVVIP, Customer name, int price) {
+    public static Customer buyProcess(Customer name, int price) {
         name.buySomething(price);
-        for (int i = 0; i < listVVIP.size(); i++) { // If Customer is in listVVIP, Do not anything
-            if(listVVIP.get(i).getID() == name.getID()) {
-                return name;
+        if (name instanceof Customer) { // If instance type is VVIPcustomer(), Doesn't execute
+            if (name.getGrade() == "VVIP") { // If customer is VVIP and not in listVVIP, insert in listVVIP and redifined with VVIPcustomer constructor for using its method
+                int index = listCustomer.indexOf(name);
+                listVVIP.add(new VVIPcustomer(name, listVVIP.size() + 1));
+                name = new VVIPcustomer(name, listVVIP.size());
+                listCustomer.set(index, name); // refer again. If i didn't do, It is no longer updated
             }
-        }
-        if (name.getGrade() == "VVIP") { // If customer is VVIP and not in listVVIP, insert in listVVIP and redifined with VVIPcustomer constructor for using its method
-            listVVIP.add(new VVIPcustomer(name, listVVIP.size() + 1));
-            name = new VVIPcustomer(name, listVVIP.size());
             return name;
         }
         else{
@@ -57,7 +65,7 @@ public class Inheritance_and_variation {
 
     public static void printList(ArrayList<Customer> list) {
         for (int i = 0; i < list.size(); i++) {
-            System.out.println((i + 1) + list.get(i).customerName + "(" + list.get(i).getGrade() + ")" + " : " + list.get(i).totalBought);
+            System.out.println((i + 1) + " : " + list.get(i).customerName + "(" + list.get(i).getGrade() + ")" + " : " + list.get(i).totalBought);
         }
     }
 }

47b1682c51b2ea23540c9a4fe551e6dde3bf4077
Jeongmo <wjdah1402@gmail.com>
2023-03-30 11:46:46 +0900

    Write main function with calculate interface

--- Chapter_10/Interface.java
+++ Chapter_10/Interface.java
@@ -6,6 +6,15 @@ package Chapter_10;
 public class Interface {
 
     public static void main(String[] args) {
-        
+        Calculator Calcul = new Calculator();
+
+        int num1 = 36;
+        int num2 = 4;
+
+        System.out.println("Add : " + Calcul.add(num1, num2));
+        System.out.println("Substract : " + Calcul.substract(num1, num2));
+        System.out.println("Times : " + Calcul.times(num1, num2));
+        System.out.println("Divides : " + Calcul.divides(num1, num2));
+        System.out.println("Divides with 0 : " + Calcul.divides(num1, 0));
     }
 }

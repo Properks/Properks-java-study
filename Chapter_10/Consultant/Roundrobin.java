4f23468945f8e0645930bf4b3007fdbaf0bf5e86
Jeongmo <wjdah1402@gmail.com>
2023-03-31 14:41:17 +0900

    Declare variable in RoundRobin

--- Chapter_10/Consultant/Roundrobin.java
+++ Chapter_10/Consultant/Roundrobin.java
@@ -1,9 +1,23 @@
 package Chapter_10.Consultant;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Roundrobin
  */
-public class Roundrobin implements Schedular{
+public class Roundrobin implements Schedular{ // Place them in order, regardless of the consultant.
+
+    int countOfConsultant;
+    int countOfCustomer;
+    int numberOfWait[]; // number of wait for each consultant.
+    Queue<Integer> queue = new LinkedList<>(); // The queue is in the order customer called.
+    
+    public Roundrobin(int countOfConsultant) {
+        this.countOfCustomer = 0;
+        this.countOfConsultant = countOfConsultant;
+        numberOfWait = new int[countOfConsultant];
+    }
 
     @Override
     public void getNextCall() {

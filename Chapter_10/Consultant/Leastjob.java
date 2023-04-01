9197a42898f7bfc5f5ed60ba5f44a3e1c649a96b
Jeongmo <wjdah1402@gmail.com>
2023-04-01 13:23:48 +0900

    Declare variable in Leastjob

--- Chapter_10/Consultant/Leastjob.java
+++ Chapter_10/Consultant/Leastjob.java
@@ -1,19 +1,33 @@
 package Chapter_10.Consultant;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Leastjob
  */
 public class Leastjob implements Schedular{
 
+    int countOfConsultant;
+    int countOfCustomer;
+    int numberOfWait[]; // number of wait for each consultant.
+    Queue<Integer> queueOfCustomer = new LinkedList<>(); // The queue is in the order customer called.
+
+    public Leastjob(int numberOfConsultant) {
+        this.countOfConsultant = numberOfConsultant;
+        countOfCustomer = 0;
+        numberOfWait = new int[countOfConsultant];
+    }
+
     @Override
     public void getNextCall() {
-        // TODO Auto-generated method stub
+        queueOfCustomer.add(++countOfCustomer);
         
     }
     
     @Override
     public void sendCallToAgent() {
-        // TODO Auto-generated method stub
+        
         
     }
 }

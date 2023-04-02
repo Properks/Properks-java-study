cf91c909c4e1d2338bcc4779d63061d532fe93f4
Jeongmo <wjdah1402@gmail.com>
2023-04-02 14:22:07 +0900

    I made a Book class to practice JDK

---
+++ Chapter_11/Book.java
@@ -0,0 +1,46 @@
+package Chapter_11;
+
+/**
+ * Book
+ */
+public class Book {
+    private String title;
+    private String author;
+    private int numPages;
+
+    public Book(String title, String author, int numPages) {
+        this.title = title;
+        this.author = author;
+        this.numPages = numPages;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getNumPages() {
+        return numPages;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setNumPages(int numPages) {
+        this.numPages = numPages;
+    }
+
+    public void printBookInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Number of pages: " + numPages);
+    }
+}

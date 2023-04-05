237002a142470deb6fe55bfc544f7abad64db9dc
Jeongmo <wjdah1402@gmail.com>
2023-04-05 23:14:37 +0900

    How to get class by using Class class

--- Chapter_11/Basic_Class_JDK.java
+++ Chapter_11/Basic_Class_JDK.java
@@ -5,21 +5,17 @@ package Chapter_11;
  */
 public class Basic_Class_JDK {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException{
         
-        Integer intValue = Integer.valueOf(42); // == Integer.parseInt("42"); like atoi(char* s);
-        Integer sameIntValue = 42; //  If intValue = Integer.valueOf("42");, sameIntValue = Integer.valueOf("42"); Result is same
-        Integer assignIntValue = intValue;
-
-        System.out.println("==========Before modify intValue==========");
-        System.out.println("intValue : " + intValue.intValue());
-        System.out.println("sameIntValue : " + sameIntValue.intValue()); 
-        System.out.println("assignIntValue : " + assignIntValue.intValue());
+        Integer intValue = Integer.valueOf(42);
         
-        intValue = 21;
-        System.out.println("==========After modify intValue==========");
-        System.out.println("intValue : " + intValue.intValue());
-        System.out.println("sameIntValue : " + sameIntValue.intValue()); // Not Reference
-        System.out.println("assignIntValue : " + assignIntValue.intValue()); // Not Reference
+        Class c = intValue.getClass();
+        System.out.println("c : " + c);
+        
+        c = Integer.class;
+        System.out.println("c : " + c);
+        
+        c = Class.forName("java.lang.Integer");
+        System.out.println("c : " + c);
     }
 }

9be621ee7fec9ded857baf90a931363924442077
Jeongmo <wjdah1402@gmail.com>
2023-04-07 14:31:06 +0900

    Fix setKeyValue function and add find function in map

--- Chapter_12/Collection_framework.java
+++ Chapter_12/Collection_framework.java
@@ -10,11 +10,19 @@ public class Collection_framework {
         Map<String, Integer> map = new Map<>();
 
         Pair<String, Integer> P = new Pair<>("John", 13);
-        // Pair<Integer, String> PPrime = new Pair<>(13,"John");
+
         map.setKeyBValue(P);
         map.setKeyBValue("Jack", 32);
-        // map.setKeyBValue(PPrime); Has type error
+        map.setKeyBValue("Amy", 14);
+        map.setKeyBValue("David", 28);
 
         map.showInfo();
+        map.processOfFind("John");; // Can find Key in map
+        
+        map.setKeyBValue("John", 23);
+        map.showInfo();
+        map.processOfFind("John"); // After change
+
+        map.findValue("Joh"); // Can't find Key in map
     }
 }
--- Chapter_12/Map.java
+++ Chapter_12/Map.java
@@ -10,12 +10,43 @@ public class Map<T, E> extends Pair<T, E>{
     ArrayList<Pair<T, E>> map = new ArrayList<>();
 
     public void setKeyBValue(Pair<T, E> p) {
+        for (int i = 0; i < map.size(); i++) {
+            if (p.key == map.get(i).key) {
+                map.get(i).value = p.value;
+                return;
+            }
+        }
         map.add(p);
     }
 
     public void setKeyBValue(T Key, E value) {
         Pair<T, E> p = new Pair<T, E>(Key, value);
+        for (int i = 0; i < map.size(); i++) {
+            if (p.key == map.get(i).key) {
+                map.get(i).value = p.value;
+                return;
+            }
+        }
         map.add(p);
+    }
+
+    public E findValue(T source) {
+        for (int i = 0; i < map.size(); i++) {
+            if (map.get(i).key == source) {
+                return map.get(i).getValue();
+            }
+        }
+        return null;
+    }
+
+    public void processOfFind(T source) {
+        E temp;
+        if ((temp = this.findValue(source)) == null) {
+            System.err.println("Can\'t find " + source);
+        }
+        else {
+            System.out.println("John is " + temp + " years old");
+        }
     }
 
     public void showInfo() {

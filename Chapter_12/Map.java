44eb686801baf16cfd55ea3052d827cb26804884
Jeongmo <wjdah1402@gmail.com>
2023-04-07 15:04:44 +0900

    Remove extends Pair because map didn't need it and add comments

--- Chapter_12/Map.java
+++ Chapter_12/Map.java
@@ -5,21 +5,21 @@ import java.util.ArrayList;
 /**
  * Map
  */
-public class Map<T, E> extends Pair<T, E>{
+public class Map<T, E> {
 
     ArrayList<Pair<T, E>> map = new ArrayList<>();
 
-    public void setKeyBValue(Pair<T, E> p) {
-        for (int i = 0; i < map.size(); i++) {
+    public void setKeyBValue(Pair<T, E> p) { // Set with pair.
+        for (int i = 0; i < map.size(); i++) { // Check whether key is already in map or not.
             if (p.key == map.get(i).key) {
                 map.get(i).value = p.value;
                 return;
             }
         }
-        map.add(p);
+        map.add(p); // Add it ArrayList called map.
     }
 
-    public void setKeyBValue(T Key, E value) {
+    public void setKeyBValue(T Key, E value) { // Set with Key and Value.
         Pair<T, E> p = new Pair<T, E>(Key, value);
         for (int i = 0; i < map.size(); i++) {
             if (p.key == map.get(i).key) {
@@ -31,17 +31,17 @@ public class Map<T, E> extends Pair<T, E>{
     }
 
     public E findValue(T source) {
-        for (int i = 0; i < map.size(); i++) {
+        for (int i = 0; i < map.size(); i++) { // Find key in map.
             if (map.get(i).key == source) {
-                return map.get(i).getValue();
+                return map.get(i).getValue(); // return value if it find key in map.
             }
         }
-        return null;
+        return null; // If it doesn't exist key in map.
     }
 
     public void processOfFind(T source) {
         E temp;
-        if ((temp = this.findValue(source)) == null) {
+        if ((temp = this.findValue(source)) == null) { // If return value of findValue is null, print error message.
             System.err.println("Can\'t find " + source);
         }
         else {
@@ -49,7 +49,7 @@ public class Map<T, E> extends Pair<T, E>{
         }
     }
 
-    public void showInfo() {
+    public void showInfo() { // print map's data.
         for (int i = 0; i < map.size(); i++) {
             if (i == map.size() - 1) {
                 System.out.println("{" + map.get(i).key + ", " + map.get(i).value + "}");
--- Chapter_12/Pair.java
+++ Chapter_12/Pair.java
@@ -8,22 +8,22 @@ public class Pair<T, E> {
     protected T key;
     protected E value;
 
-    public Pair() {
+    public Pair() { // Constructor.
         this.key = null;
         this.value = null;
     }
 
-    public Pair(T key, E value) {
+    public Pair(T key, E value) { // Constructor with Key and Value
         this.key = key;
         this.value = value;
     }
 
-    public void set(T key, E value) {
+    public void set(T key, E value) { // Set Key and Value
         this.key = key;
         this.value = value;
     }
 
-    public E getValue() {
+    public E getValue() { // return Value in order to find Key in map
         return value;
     }
 }

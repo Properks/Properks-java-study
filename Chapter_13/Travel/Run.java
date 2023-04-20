b49c61e4a1e3a69fc10aef2cc39b32fa3658ef0e
Jeongmo <wjdah1402@gmail.com>
2023-04-20 15:23:05 +0900

    Calculate fee in order to travel

---
+++ Chapter_13/Travel/Run.java
@@ -0,0 +1,11 @@
+package Chapter_13.Travel;
+
+/**
+ * Run
+ */
+public class Run {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Chapter_13/Travel/Traveler.java
@@ -0,0 +1,28 @@
+package Chapter_13.Travel;
+
+/**
+ * Traveler
+ */
+public class Traveler {
+
+    private String name;
+    private int age;
+
+    public Traveler() {
+        name = null;
+        age = 0;
+    }
+    
+    public Traveler(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}

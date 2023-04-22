642edf3edb66790e11cc78659020108b7f787b73
Jeongmo <wjdah1402@gmail.com>
2023-04-22 12:42:52 +0900

    Make Exception file

---
+++ Chapter_14/Exception.java
@@ -0,0 +1,12 @@
+package Chapter_14;
+
+/**
+ * Exception
+ */
+public class Exception {
+
+    // FileNotFoundException
+    // SocketException
+    // ArithmeticException
+    // IndexOutOfBoundsException
+}

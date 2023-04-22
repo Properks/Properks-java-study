ec79834578740bd39a9e2ef6939edd1dd57f7410
Jeongmo <wjdah1402@gmail.com>
2023-04-22 13:00:00 +0900

    Basic of Exception handling

--- Chapter_14/Exception.java
+++ Chapter_14/Exception.java
@@ -9,4 +9,21 @@ public class Exception {
     // SocketException
     // ArithmeticException
     // IndexOutOfBoundsException
+    public static void main(String[] args) {
+
+        int[] arr = new int[5];
+
+        try {
+            for (int i = 0; i <= 5; i++) {
+                arr[i] = i;
+                
+            }
+            System.out.println("This doesn't work");
+        } 
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e);
+            System.out.println("Exception handling");
+        }
+        System.out.println("System exit");
+    }
 }

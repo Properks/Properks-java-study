16b583430252f662109c46b8c207a22793640c43
Jeongmo <wjdah1402@gmail.com>
2023-04-23 14:42:22 +0900

    Try-catch-finally statement

--- Chapter_14/Exception.java
+++ Chapter_14/Exception.java
@@ -2,6 +2,7 @@ package Chapter_14;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  * Exception
@@ -14,12 +15,24 @@ public class Exception {
     // IndexOutOfBoundsException
     public static void main(String[] args) {
 
+        FileInputStream file = null;
         try {
-            FileInputStream file = new FileInputStream("a.txt");
+            file = new FileInputStream("a.txt");
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            // System.out.println(e);
+            // e.printStackTrace();
+            System.out.println(e);
             return;
         }
+        finally {
+            if (file != null) {
+                try {
+                    file.close();
+                } catch (IOException e) { // It doesn't run
+                    System.out.println(e);
+                }
+            }
+            System.out.println("Run this in finally"); // Print it regardless of whether return statement in catch run or not
+        }
+        System.out.println("Run this in main and out of try"); //Cannot run if catch run.
     }
 }

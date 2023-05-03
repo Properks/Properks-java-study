cc9e8eb24dd2f7788b436f7adc7db89824c6d143
Jeongmo <wjdah1402@gmail.com>
2023-05-03 13:09:12 +0900

    Input int, double and string with scanner

--- Chapter_15/Java_Input_Output.java
+++ Chapter_15/Java_Input_Output.java
@@ -1,14 +1,6 @@
 package Chapter_15;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Java_Input_Output
@@ -16,41 +8,23 @@ import java.io.IOException;
 public class Java_Input_Output {
 
     public static void main(String[] args) {
-        // READ BYTE
-        // FileInputStream
-        // FileOutputStream
-        // BufferedInputStream
-        // BufferedOutputStream
+        String name;
+        int age;
+        double grade;
 
-        // READ CHARACTER
-        // FileReader
-        // FileWriter
-        // BufferedReader
-        // BufferedWriter
-        int i;
-        System.out.println("Enter one character");
+        try (Scanner scanner = new Scanner(System.in)) { // In order to close
+            System.out.println("Enter a name : ");
+            name = scanner.nextLine();
 
-        try {
-            i = System.in.read(); // Read 1 byte with read()
-            System.out.println("Entered character : " + (char)i); // If don't use (char), print ASCII code.
-        } catch (IOException e) { // for exception
-            e.printStackTrace();
+            System.out.println("Enter a age : ");
+            age = scanner.nextInt();
+
+            System.out.println("Enter a grade(0.0 ~ 4.5) : ");
+            grade = scanner.nextDouble();
         }
 
-        System.out.println("Enter a word");
-
-        try {
-            // i = System.in.read(); // for printing "Entered word : "
-            // System.out.print("Entered word : ");
-            // do {
-            //     System.out.print((char)i);
-            // } while ((i = System.in.read()) != -1);
-            while ((i = System.in.read()) != -1) { // input with while
-                System.out.print((char)i);
-            }
-            
-        } catch (IOException e) {
-            e.printStackTrace();;
-        }
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("Grade : " + grade);
     }
 }

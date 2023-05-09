7df28a05673aafdc9dc0f04af6ac2e565836938f
Jeongmo <wjdah1402@gmail.com>
2023-05-09 11:14:21 +0900

    Apply read() method in while statement

--- Chapter_15/Java_Input_Output.java
+++ Chapter_15/Java_Input_Output.java
@@ -11,10 +11,34 @@ public class Java_Input_Output {
     public static void main(String[] args) {
 
         FileInputStream filestream = null;
-
+        int i; // input 
         try {
-            filestream = new FileInputStream("Chapter_15/Input.txt");
-            System.out.println((char)filestream.read());
+            filestream = new FileInputStream("Chapter_15/Input.txt"); // print word
+            System.out.println("Word : ");
+            while ((i = filestream.read()) != ' ') {
+                System.out.print((char)i);
+            }
+            System.out.println();
+            System.out.println();
+            
+            System.out.println("Sentence : ");
+            filestream.close();
+            filestream = new FileInputStream("Chapter_15/Input.txt"); // print sentence
+
+            while ((i = filestream.read()) != '\n' && i != '.' && i != '?' && i != '!') {
+                System.out.print((char)i);
+            }
+            System.out.println();
+            System.out.println();
+            
+            System.out.println("All : ");
+            filestream.close();
+            filestream = new FileInputStream("Chapter_15/Input.txt"); // print all
+            
+            while ((i = filestream.read()) != -1) {
+                System.out.print((char)i);
+            }
+            System.out.println();
         } 
         catch (IOException e) {
             System.out.println(e);

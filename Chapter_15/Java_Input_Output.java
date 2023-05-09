ad1e240e85d684cf8c2e98ccc873208e8062b890
Jeongmo <wjdah1402@gmail.com>
2023-05-09 11:01:24 +0900

    Basic of FileInputStream

--- Chapter_15/Java_Input_Output.java
+++ Chapter_15/Java_Input_Output.java
@@ -1,7 +1,7 @@
 package Chapter_15;
 
-import java.io.Console;
-import java.util.Arrays;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 /**
  * Java_Input_Output
@@ -9,39 +9,15 @@ import java.util.Arrays;
 public class Java_Input_Output {
 
     public static void main(String[] args) {
-        String ID;
-        char[] password;
-        char[] temp;
 
-        Console console = System.console();// In order to close
-        System.out.println("Enter a ID : ");
-        ID = console.readLine();
-
-        System.out.println("Enter a password (more than 8): ");
-        password = console.readPassword();
-        
-        System.out.println("Enter a password one more : ");
-        temp = console.readPassword();
+        FileInputStream filestream = null;
 
         try {
-            if (!Arrays.equals(password, temp)) {
-                throw new Exception("Password entered first and second isn't same");
-            }
-            else if (password.length < 8) {
-                throw new Exception("Password is less than 8");
-            }
-            System.out.println("Create account successfully");
-            System.out.println("Entered ID is " + ID);
-        } catch(Exception e) {
-            e.printStackTrace();
-            System.out.print("Password entered first is ");
-            for (int i = 0; i < password.length; i++) {
-                System.out.print((i == password.length - 1) ? password[i] + "\n" : password[i]);
-            }
-            System.out.print("Password entered second is ");
-            for (int i = 0; i < temp.length; i++) {
-                System.out.print((i == temp.length - 1) ? temp[i] + "\n" : temp[i]);
-            }
+            filestream = new FileInputStream("Chapter_15/Input.txt");
+            System.out.println((char)filestream.read());
+        } 
+        catch (IOException e) {
+            System.out.println(e);
         }
     }
 }

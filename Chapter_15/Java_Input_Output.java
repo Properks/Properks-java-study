e0f4326d1d0af1d0edabdffffe75dc1746ab8cef
Jeongmo <wjdah1402@gmail.com>
2023-05-23 11:31:55 +0900

    Compare time between FileStream and BufferedStream

--- Chapter_15/Java_Input_Output.java
+++ Chapter_15/Java_Input_Output.java
@@ -1,9 +1,9 @@
 package Chapter_15;
 
-import java.io.Console;
-import java.io.File;
-import java.io.InputStreamReader;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 /**
  * Java_Input_Output
@@ -12,18 +12,34 @@ public class Java_Input_Output {
 
     public static void main(String[] args) {
 
-        Console Inputconsole = System.console();
-        String buffer = Inputconsole.readLine();
-        File Filename = new File("Chapter_15/" + buffer);
+        long second = 0;
         
-        try (InputStreamReader reader = new InputStreamReader(new FileInputStream(Filename))) { // InputStreamReader read it with character data form, FileInputStream read it with byte type
+        try (FileInputStream basefile = new FileInputStream("Chapter_15/base.zip");
+        FileOutputStream duplicatedFile = new FileOutputStream("Chapter_15/copy.zip")) { // InputStreamReader read it with character data form, FileInputStream read it with byte type
+            second = System.currentTimeMillis();
             int i;
-            while ((i = reader.read()) != -1) {
-                System.out.print((char)i);
+            while ((i = basefile.read()) != -1) {
+                duplicatedFile.write(i);
             }
+            second = System.currentTimeMillis() - second;
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
+        System.out.println("Time : " + second); // print about 899
+
+       try (FileInputStream basefile = new FileInputStream("Chapter_15/base.zip");
+        FileOutputStream duplicatedFile = new FileOutputStream("Chapter_15/copy.zip")) { // InputStreamReader read it with character data form, FileInputStream read it with byte type
+            BufferedInputStream base = new BufferedInputStream(basefile);
+            BufferedOutputStream duplicate = new BufferedOutputStream(duplicatedFile);
+            second = System.currentTimeMillis();
+            int i;
+            while ((i = base.read()) != -1) {
+                duplicate.write(i);
+            }
+            second = System.currentTimeMillis() - second;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Time : " + second); // print about 26
     }
 }

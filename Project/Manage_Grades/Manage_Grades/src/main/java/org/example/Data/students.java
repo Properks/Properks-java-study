35e910158003c9bea1b714e0b4c1847943d1ba0a
Jeongmo <wjdah1402@gmail.com>
2023-05-31 14:36:19 +0900

    [Manage_Grade] Students data file for run

---
+++ Project/Manage_Grades/Manage_Grades/src/main/java/org/example/Data/students.java
@@ -0,0 +1,44 @@
+package org.example.Data;
+
+import org.example.Class.Students;
+import org.example.Constants.commonconstants;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+public class students {
+    public static final Set<Students> list = Set.of(
+            new Students("Andrew", commonconstants.MajorList.get(0)),
+            new Students("Asher", commonconstants.MajorList.get(1)),
+            new Students("Benjamin", commonconstants.MajorList.get(2)),
+            new Students("Eric", commonconstants.MajorList.get(0)),
+            new Students("George", commonconstants.MajorList.get(2)),
+            new Students("Henry", commonconstants.MajorList.get(0)),
+            new Students("Harry", commonconstants.MajorList.get(1)),
+            new Students("James", commonconstants.MajorList.get(1)),
+            new Students("Brian", commonconstants.MajorList.get(6)),
+            new Students("Brook", commonconstants.MajorList.get(5)),
+            new Students("Cherry", commonconstants.MajorList.get(6)),
+            new Students("Emilly", commonconstants.MajorList.get(3)),
+            new Students("Auther", commonconstants.MajorList.get(4)),
+            new Students("Katie", commonconstants.MajorList.get(3)),
+            new Students("Melissa", commonconstants.MajorList.get(4)),
+            new Students("Tracy", commonconstants.MajorList.get(4)),
+            new Students("Dorothy", commonconstants.MajorList.get(0)),
+            new Students("Brian", commonconstants.MajorList.get(5)),
+            new Students("Alice", commonconstants.MajorList.get(0)),
+            new Students("Lisa", commonconstants.MajorList.get(2)),
+            new Students("Evelyn", commonconstants.MajorList.get(5)),
+            new Students("Jinny", commonconstants.MajorList.get(0)),
+            new Students("Jessica", commonconstants.MajorList.get(0)),
+            new Students("Jennifer", commonconstants.MajorList.get(0)),
+            new Students("Helen", commonconstants.MajorList.get(5)),
+            new Students("Katelyn", commonconstants.MajorList.get(6)),
+            new Students("Natalie", commonconstants.MajorList.get(6)),
+            new Students("Nicole", commonconstants.MajorList.get(4)),
+            new Students("Paula", commonconstants.MajorList.get(3)),
+            new Students("Victoria", commonconstants.MajorList.get(2)),
+            new Students("Sarah", commonconstants.MajorList.get(4))
+    );
+}

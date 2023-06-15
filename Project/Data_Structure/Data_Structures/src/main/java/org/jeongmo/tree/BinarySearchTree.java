37a786f510881ac9df24a3f0dcf538e5549936c5
Jeongmo <wjdah1402@gmail.com>
2023-06-15 14:26:46 +0900

    [Data_Structures] Test search function in BST and Fix it

--- Project/Data_Structure/Data_Structures/src/main/java/org/jeongmo/tree/BinarySearchTree.java
+++ Project/Data_Structure/Data_Structures/src/main/java/org/jeongmo/tree/BinarySearchTree.java
@@ -4,12 +4,16 @@ import java.util.logging.Logger;
 
 public class BinarySearchTree {
 
+    private static final Logger logger = Logger.getLogger(BinarySearchTree.class.getName());
     public static void main(String[] args) {
         BinarySearchTree bst = new BinarySearchTree(27);
         bst.root = bst.addElement(bst.getRoot(), 20);
         bst.root = bst.addElement(bst.getRoot(), 10);
         bst.root = bst.addElement(bst.getRoot(), 14);
         bst.root = bst.addElement(bst.getRoot(), 32);
+        if (bst.search(bst.getRoot(), 14)) {
+            logger.info("14 is in tree");
+        }
         Tree.inorder(bst.getRoot());
 
     }
@@ -41,7 +45,7 @@ public class BinarySearchTree {
         }
         if (tree.value == value) {
             return true;
-        } else if (tree.value > value) {
+        } else if (tree.value < value) {
             return search(tree.right, value);
         } else {
             return search(tree.left, value);

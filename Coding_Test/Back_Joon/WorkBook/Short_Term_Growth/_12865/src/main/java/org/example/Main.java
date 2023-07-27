7b24c4d8839fd1ce93c9170dd8f3bc8e0f56a246
Jeongmo <wjdah1402@gmail.com>
2023-07-27 14:21:46 +0900

    :art: It has answer that I want, but Time Over.

--- Coding_Test/Back_Joon/WorkBook/Short_Term_Growth/_12865/src/main/java/org/example/Main.java
+++ Coding_Test/Back_Joon/WorkBook/Short_Term_Growth/_12865/src/main/java/org/example/Main.java
@@ -1,7 +1,69 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    static int maxValuable = 0;
+    static Luggage[] luggages;
+    static int number;
+    static int totalWeight;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+
+        number = Integer.parseInt(stringTokenizer.nextToken());
+        totalWeight = Integer.parseInt(stringTokenizer.nextToken());
+
+        luggages = new Luggage[number];
+
+        for (int i = 0; i < number; i++) {
+            stringTokenizer = new StringTokenizer(br.readLine());
+            luggages[i] = new Luggage(Integer.parseInt(stringTokenizer.nextToken()),
+                    Integer.parseInt(stringTokenizer.nextToken()));
+        }
+
+        System.out.println(getMaxValuable(0, 0, 0));
+
+    }
+
+    static class Luggage implements Comparable<Luggage>{
+        private int weight;
+        private int valuable;
+
+        public Luggage(int weight, int valuable) {
+            this.weight = weight;
+            this.valuable = valuable;
+        }
+
+        public int getWeight() {
+            return this.weight;
+        }
+
+        public int getValuable() {
+            return this.valuable;
+        }
+
+        @Override
+        public int compareTo(Luggage o) {
+            return this.valuable - o.getValuable();
+        }
+    }
+
+    private static int getMaxValuable(int index, int weight, int sum) {
+        if (index >= number) {
+            return sum;
+        }
+
+        if (weight + luggages[index].getWeight() > totalWeight) {
+            return getMaxValuable(index + 1, weight, sum);
+        } else {
+            return Math.max(getMaxValuable(index+1, weight, sum),
+                    getMaxValuable(index + 1,
+                            weight + luggages[index].getWeight(),
+                            sum + luggages[index].getValuable()));
+        }
     }
 }

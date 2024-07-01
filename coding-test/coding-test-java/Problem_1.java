d4d93085a19a70b0fd8e47fd8c74d912e5b27476
Jeongmo Seo <wjdah1402@gmail.com>
2024-07-01 21:20:35 +0900

    :sparkles: feat: 배열 정렬 및 중복 값 제거 문제 풀이 - 문제1 풀이, 배열 정렬 - 문제2 풀이, 배열 정렬 및 중복 값 제거

---
+++ coding-test/coding-test-java/Problem_1.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Problem_1 {
+
+    public static void main(String[] args) {
+
+        int[] input = new int[100000];
+//        int[] result = solution(input);
+//        System.out.println(result.toString());
+
+        long start = System.currentTimeMillis();
+        int[] bubble = bubbleSort(input);
+        long end = System.currentTimeMillis();
+        System.out.println((end - start) / 1000d + "초"); // O(n^2) 알고리즘은 대략 1초 (n:1,000,000 -> 0.005초)
+
+        start = System.currentTimeMillis();
+        int[] solution = solution2(input);
+        end = System.currentTimeMillis();
+        System.out.println((end - start) / 1000d + "초"); // O(nlong) 알고리즘은 대략 0.001초 (n:1,000,000 -> 89초)
+
+    }
+
+    /**
+     * 기본 배열을 정렬해서 반환, 반환값을 void로 놓고 함수 실행만 해서 구현해도 원래 배열이 정렬됨
+     * @param array 정렬할 배열
+     * @return 정렬된 배열
+     */
+    public static int[] solution(int[] array) {
+        Arrays.sort(array); // Arrays.sort()는 원본 배열을 정렬해서 원본 배열에 반영
+        return array;
+    }
+
+    /**
+     * 입력된 배열은 그대로 두고 새로운 정렬된 배열 반환
+     * @param array 정렬할 배열
+     * @return 정렬된 배열
+     */
+    public static int[] solution2(int[] array) {
+        int[] clone = array.clone();
+        Arrays.sort(clone); // O(nlogn): Dual pivot quick sort
+        return clone;
+    }
+
+    public static int[] bubbleSort(int[] array) {
+        int[] clone = array.clone();
+        for (int i = 0; i < clone.length; i++) {
+            for (int j = 0; j < clone.length - i - 1; j++) {
+                if (clone[j + 1] < clone[j]) {
+                    int temp = clone[j + 1];
+                    clone[j + 1] = clone[j];
+                    clone[j] = temp;
+                }
+            }
+        }
+        return clone;
+    }
+}
---
+++ coding-test/coding-test-java/Problem_2.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class Problem_2 {
+    public static void main(String[] args) {
+        int[] input1 = generateArrayWithDuplicates(100000);
+        int[] input2 = generateArrayWithDuplicates(100000);
+
+        List<Integer> result1 = new ArrayList<>(Arrays.stream(input1).boxed().toList());
+        List<Integer> result2 = new ArrayList<>(Arrays.stream(input2).boxed().toList());
+
+
+        long start = System.currentTimeMillis();
+        Collections.sort(result1);
+        Collections.sort(result2);
+
+        result1 = solution(result1);
+        result2 = solution(result2);
+
+//        System.out.println(result1);
+//        System.out.println(result2);
+        long end = System.currentTimeMillis();
+        System.out.println("First algorithm: " + ((end - start) / 1000d) + "초"); // 대략 0.3초
+
+
+        // 풀이
+        start = System.currentTimeMillis();
+        List<Integer> result3 = new ArrayList(Arrays.stream(input1).boxed().distinct().toList());
+        // boxed(): int -> Integer로 변환
+        // distinct(): 중복값 삭제
+
+        result3.sort(Collections.reverseOrder()); // reversedOrder(): 내림차순 정렬을 위한 Comparator
+//        System.out.println(result3);
+
+        List<Integer> result4 = new ArrayList(Arrays.stream(input2).boxed().distinct().toList());
+        result4.sort(Collections.reverseOrder());
+//        System.out.println(result4);
+        end = System.currentTimeMillis();
+        System.out.println("Second algorithm: " + ((end - start) / 1000d) + "초"); // 대략 0.1초
+
+    }
+
+    public static List<Integer> solution(List<Integer> list) {
+        List<Integer> clone = new ArrayList<>(list);
+        int i = 0;
+        int n = list.size();
+        while (i < n) {
+            if (i != clone.size() - 1 && clone.get(i).equals(clone.get(i + 1))) {
+                clone.remove(i + 1);
+                n = clone.size();
+                continue;
+            }
+            i++;
+        }
+        return clone;
+    }
+
+    // input 생성
+    private static int[] generateArrayWithDuplicates(int size) {
+        Random random = new Random();
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(200001) - 100000; // -100,000부터 100,000 사이의 랜덤 값
+        }
+        return array;
+    }
+}

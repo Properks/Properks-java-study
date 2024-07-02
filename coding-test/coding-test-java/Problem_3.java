20d1e902bd352d8d4311195f21d95fe303fa9ffe
Jeongmo Seo <wjdah1402@gmail.com>
2024-07-02 21:05:11 +0900

    :sparkles: feat: 중복 없이 정렬하는 배열, 최댓값 문제 풀이 - 문제 3 풀이, 중복 없이 배열 정렬하기 - 문제 4 풀이, 최댓값끼리 배열로 반환하기

---
+++ coding-test/coding-test-java/Problem_3.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+// 배열에서 두 수를 뽑아 더해서 오름차순으로 정렬
+public class Problem_3 {
+    public static void main(String[] args) {
+        int[] input1 = {2, 1, 3, 4, 1};
+        int[] input2 = {5, 0, 2, 7};
+
+        //내 풀이
+        System.out.println("내 풀이");
+        Set<Integer> result1 = solution(input1);
+        Set<Integer> result2 = solution(input2);
+
+        System.out.println(result1);
+        System.out.println(result2);
+
+        //책 풀이
+        System.out.println("책 풀이");
+        int[] result3 = solution1(input1);
+        int[] result4 = solution1(input2);
+
+        System.out.println(result3);
+        System.out.println(result4);
+    }
+
+    // 내 풀이
+    public static Set<Integer> solution(int[] input) {
+        Set<Integer> set = new TreeSet<>(); // 역순이면 new TreeSet<>(Comparator.reversedOrder());
+
+        for (int i = 0; i < input.length - 1; i++) {
+            for (int j = i + 1; j < input.length; j++) {
+                set.add(input[i] + input[j]);
+            }
+        }
+
+        return set;
+    }
+
+    // 책 풀이
+    public static int[] solution1(int[] input) {
+        Set<Integer> set = new HashSet();
+
+        for (int i = 0; i < input.length - 1; i++) {
+            for (int j = i + 1; j < input.length; j++) {
+                set.add(input[i] + input[j]);
+            }
+        }
+        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
+    }
+}
---
+++ coding-test/coding-test-java/Problem_4.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+// 3명이 일정하게 답을 찍은 경우 가장 많이 맞은 사람을 배열로 반환
+public class Problem_4 {
+
+    public static void main(String[] args) {
+
+        int[][] pattern = {
+                {1, 2, 3, 4, 5},
+                {2, 1, 2, 3, 2, 4, 2, 5},
+                {2, 1, 2, 3, 2, 4, 2, 5}
+        };
+
+        int[] answer1 = {1, 2, 3, 4, 5, 3, 3, 2, 1, 4};
+        int[] answer2 = {1, 3, 2, 4, 2, 5, 3, 2, 2, 1, 3, 4, 2};
+
+        int[] result1 = solution(answer1, pattern);
+        int[] result2 = solution(answer2, pattern);
+
+        System.out.println(Arrays.stream(result1).boxed().toList());
+        System.out.println(Arrays.stream(result2).boxed().toList());
+    }
+
+    public static int[] solution(int[] answers, int[][] pattern) {
+
+        int[] corrected = new int[3];
+        for (int i = 0; i < answers.length; i++) {
+            for (int j = 0; j < pattern.length; j++) {
+                if (pattern[j][i >= pattern[j].length ? i % pattern[j].length : i] == answers[i]) {
+                    corrected[j]++;
+                }
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        int max = Math.max(corrected[0], Math.max(corrected[1], corrected[2]));
+
+        // 책에서 최대를 구하는 방식
+        max = Arrays.stream(corrected).max().getAsInt(); // max() in Stream -> stream().reduce(Math:max)
+
+        // List<Integer> 형태일 때 최대를 구하는 방법
+//        max = Collections.max(Arrays.stream(corrected).boxed().toList());
+
+        for (int i = 0; i < 3; i++) {
+            if (max == corrected[i]) {
+                result.add(i + 1);
+            }
+        }
+        return result.stream().sorted().mapToInt(Integer::intValue).toArray();
+    }
+}

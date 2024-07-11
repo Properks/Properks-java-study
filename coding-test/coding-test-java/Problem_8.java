41c766991653ae75ee20bd59e78472dad89677a5
Jeongmo Seo <wjdah1402@gmail.com>
2024-07-11 17:11:57 +0900

    :sparkles: 괄호 및 2진수 문제 풀이 - 문제 8 풀이, 스택으로 괄호 쌍이 맞는지 확인 - 문제 9 풀이, 스택으로 이진수 구하는 문제

---
+++ coding-test/coding-test-java/Problem_8.java
@@ -0,0 +1,85 @@
+import java.util.*;
+
+// 괄호가 쌍이 맞는지 확인하는 문제
+public class Problem_8 {
+
+    public static void main(String[] args) {
+        String input1 = "()()";
+        String input2 = "(())()";
+        String input3 = ")()(";
+        String input4 = "(()(";
+
+        boolean result1 = solution(input1);
+        boolean result2 = solution(input2);
+        boolean result3 = solution(input3);
+        boolean result4 = solution(input4);
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+
+
+        boolean result5 = solution1(input1);
+        boolean result6 = solution1(input2);
+        boolean result7 = solution1(input3);
+        boolean result8 = solution1(input4);
+
+        System.out.println(result5);
+        System.out.println(result6);
+        System.out.println(result7);
+        System.out.println(result8);
+    }
+
+    // 내 풀이
+    public static boolean solution(String input) {
+        Stack<Character> stack = new Stack();
+        int count = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        // stack.size()로 하면 로직 도중 pop하면서 크기가 변경되어 로직이 중간에 끊긴다.
+        for (int i = 0; i < input.length(); i++) {
+            char bracket = stack.pop();
+
+            if (bracket == '(') {
+                count--;
+            }
+
+            else if (bracket == ')') {
+                count++;
+            }
+
+            if (count < 0) {
+                return false;
+            }
+        }
+
+        return count == 0;
+    }
+
+    // 책 풀이
+    // 여는 괄호만 넣고 닿는 괄호는 stack에서 여는 괄호를 빼서 삭제한다.
+    public static boolean solution1(String input) {
+        Stack<Character> stack = new Stack();
+
+        for (int i = 0; i < input.length(); i++) {
+            char bracket = input.charAt(i);
+
+            if (bracket == '(') {
+                stack.push(bracket);
+            }
+
+            else if (bracket == ')' && !stack.isEmpty()) {
+                stack.pop();
+            }
+
+            else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
---
+++ coding-test/coding-test-java/Problem_9.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+// 10진수를 2진수로 변형하는 과정
+public class Problem_9 {
+
+    public static void main(String[] args) {
+        int input1 = 10;
+        int input2 = 27;
+        int input3 = 12345;
+
+        String result1 = solution(input1);
+        String result2 = solution(input2);
+        String result3 = solution(input3);
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+    }
+
+    public static String solution(int input) {
+        Stack<Integer> stack = new Stack();
+
+        int i = 2;
+        // 책에서는 조건은 input > 0 형태로 함
+        while (input != 0){
+            stack.push(input % i);
+            input /= i;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        return sb.toString();
+    }
+
+}

d292bcbb15a7167f65583401e2372baa05d24100
Jeongmo Seo <wjdah1402@gmail.com>
2024-07-18 19:49:25 +0900

    :sparkles: 데이터 삭제, 복구, 이동 문제 풀이 - 문제 14 풀이, 입력으로 인한 삭제, 복구, 이동을 스택을 이용하여 풀이

---
+++ coding-test/coding-test-java/Problem_14.java
@@ -0,0 +1,94 @@
+import java.util.*;
+
+// 데이터 행을 이동 삭제 복구 작업
+public class Problem_14 {
+
+    public static void main(String[] args) {
+        int row1 = 8;
+        int index1 = 2;
+        String[] board = {
+                "무지",
+                "콘",
+                "어피치",
+                "제이지",
+                "프로도",
+                "네오",
+                "튜브",
+                "라이언"
+        };
+        String[] input1 = {
+                "D 2",
+                "C",
+                "U 3",
+                "C",
+                "D 4",
+                "C",
+                "U 2",
+                "Z",
+                "Z"
+        };
+        String[] input2 = {
+                "D 2",
+                "C",
+                "U 3",
+                "C",
+                "D 4",
+                "C",
+                "U 2",
+                "Z",
+                "Z",
+                "U 1",
+                "C"
+        };
+
+        String result1 = solution(board, row1, index1, input1);
+        String result2 = solution(board, row1, index1, input2);
+
+        System.out.println(result1);
+        System.out.println(result2);
+
+    }
+
+    // 구현 순서 분석
+    // 1. input을 보고 명령어 대로 clone 배열과 removed 스택에 처리하기
+    // 1-1. 삭제, 되돌리기, 이동
+    // 2. 원래 배열인 String[] 과 List<String>을 인덱스를 사용하여 비교
+    public static String solution(String[] board, int row, int index, String[] inputs) {
+        List<String> clone = new ArrayList<>(Arrays.stream(board).toList());
+        Stack<List<Object>> removed = new Stack<>();
+        for (String input: inputs) {
+            if (input.startsWith("D")) {
+                int value = Integer.parseInt(input.substring(2));
+                if (index + value < row - 1) {
+                    index += value;
+                }
+            }
+            else if (input.startsWith("U")) {
+                int value = Integer.parseInt(input.substring(2));
+                if (index - value >= 0) {
+                    index -= value;
+                }
+            }
+            else if (input.equals("C")) {
+                removed.push(List.of(index, clone.remove(index)));
+            }
+            else if (input.equals("Z") && !removed.isEmpty()) {
+                List<Object> value = removed.pop();
+                clone.add((int) value.get(0), String.valueOf(value.get(1)));
+            }
+        }
+
+        int cloneIndex = 0;
+        StringBuilder sb = new StringBuilder();
+        for (String value : board) {
+            if (clone.get(cloneIndex).equals(value)) {
+                sb.append("O");
+                cloneIndex++;
+            }
+            else {
+                sb.append("X");
+            }
+        }
+        return sb.toString();
+    }
+}

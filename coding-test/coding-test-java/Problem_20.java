77200a0c032fc9091f35c9e4657325a730176d56
Jeongmo Seo <wjdah1402@gmail.com>
2024-07-23 14:48:16 +0900

    :sparkles: 10일 연속 할인 받을 수 있는 날짜 수 찾는 문제 풀이 - 문제 20 풀이, hash를 이용하여 할인 가능 날짜 찾기

---
+++ coding-test/coding-test-java/Problem_20.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+// 10일 연속으로 할인하는 날짜 찾기
+public class Problem_20 {
+
+    public static void main(String[] args) {
+        String[] want1 = {"banana", "apple", "rice", "pork" , "pot"};
+        String[] want2 = {"apple"};
+
+        int[] number1 = {3, 2, 2, 2, 1};
+        int[] number2 = {10};
+
+        String[] discount1 = {"chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"};
+        String[] discount2 = {"banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana"};
+
+        int result1 = solution(want1, number1, discount1);
+        int result2 = solution(want2, number2, discount2);
+
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+
+    public static int solution(String[] want, int[] number, String[] discount) {
+        Map<String, Integer> count = new HashMap<>();
+        for (int i = 0; i < want.length; i++) {
+            count.put(want[i], number[i]);
+        }
+
+        int answer = 0;
+        out: for (int i = 0; i < discount.length - 9; i++) {
+            Map<String, Integer> hash = new HashMap<>(count);
+            // 책에서는 Map을 생성하고 1씩 추가하며 넣어서 count.equals(hash);로 확인한다.
+            for (int j = i; j < i + 10; j++) {
+                if (!hash.containsKey(discount[j]) || hash.get(discount[j]) <= 0) {
+                    continue out;
+                }
+                hash.put(discount[j], hash.get(discount[j]) - 1);
+            }
+
+            int max = hash.values().stream().mapToInt(Integer::intValue).max().getAsInt();
+            if (max == 0) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}

99fcad45ce12ee55bfbdea7ecc7822008862778c
Jeongmo Seo <wjdah1402@gmail.com>
2024-11-23 15:58:05 +0900

    :sparkles: 문제 24 재풀이 실패 - dfs를 이용한 조합, map의 stream 이용 미숙

--- coding-test/coding-test-java/Problem_24.java
+++ coding-test/coding-test-java/Problem_24.java
@@ -14,7 +14,7 @@ public class Problem_24 {
         String[] orders3 = {"XYZ", "XWY", "WXA"};
         int[] course3 = {2, 3, 4};
 
-        String[] result1 = solution(orders1, course1);
+        String[] result1 = solution1(orders1, course1);
         String[] result2 = solution(orders2, course2);
         String[] result3 = solution(orders3, course3);
 
@@ -74,6 +74,54 @@ public class Problem_24 {
             for (int i = start; i < array.length; i++) {
                 visited[i] = true;
                 combination(result, array, visited, i + 1, r - 1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    // 재풀이 (실패)
+    // 정답을 추출하는 부분의 Stream 다시 한번 생각해보기
+    public static String[] solution1(String[] orders, int[] course) {
+        Map<String, Integer> count = new HashMap<>();
+        boolean[] visited;
+
+        for (String order : orders) {
+            for (int index : course) {
+                if (order.length() < index) {
+                    continue;
+                }
+                visited = new boolean[order.length()];
+                combination(count, visited, order, 0, index);
+            }
+        }
+
+        List<String> answer = new ArrayList<>();
+
+        for (int index : course) {
+            count.entrySet().stream().filter(o -> o.getKey().length() == index).max((o1, o2) -> o1.getValue().compareTo(o2.getValue())).ifPresent(max -> count.entrySet().stream().filter(o -> o.getValue().equals(max.getValue()) && max.getValue() > 1).forEach(o -> answer.add(o.getKey())));
+        }
+
+        return answer.stream().sorted().toArray(String[]::new);
+    }
+
+    public static void combination(Map<String, Integer> count,
+                                   boolean[] visited,
+                                   String order,
+                                   int start,
+                                   int index) {
+        if (index == 0) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < visited.length; i++) {
+                if (visited[i]) {
+                    sb.append(order.toCharArray()[i]);
+                }
+            }
+            count.put(sb.toString(), count.getOrDefault(sb.toString(), 0) + 1);
+        }
+        else {
+            for (int i = start; i < order.length(); i++) {
+                visited[i] = true;
+                combination(count, visited, order, i + 1, index - 1);
                 visited[i] = false;
             }
         }

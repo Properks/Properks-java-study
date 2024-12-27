b1a822b53a3db635c74bb26603b71dc138b25af4
Jeongmo Seo <wjdah1402@gmail.com>
2024-12-27 16:09:30 +0900

    :sparkles: 문제 42 문제 풀이 성공 - DFS로 풀이

--- coding-test/coding-test-java/Problem_42.java
+++ coding-test/coding-test-java/Problem_42.java
@@ -16,6 +16,16 @@ public class Problem_42 {
         System.out.println(result1);
         System.out.println(result2);
         System.out.println(result3);
+
+        System.out.println("DFS");
+
+        int result4 = solutionWithDFS(n1, wires1);
+        int result5 = solutionWithDFS(n2, wires2);
+        int result6 = solutionWithDFS(n3, wires3);
+
+        System.out.println(result4);
+        System.out.println(result5);
+        System.out.println(result6);
     }
 
     public static int solution(int n, int[][] wires) {
@@ -42,5 +52,40 @@ public class Problem_42 {
             min = Math.min(min, Math.abs(set1.size() - set2.size()));
         }
         return min;
+    }
+
+    public static int solutionWithDFS(int n, int[][] wires) {
+        List<Integer>[] nodes = new ArrayList[n + 1];
+        boolean[] visited = new boolean[n + 1];
+        int answer = n;
+        for (int i = 0; i < n + 1; i++) {
+            nodes[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < wires.length; i++) {
+            nodes[wires[i][0]].add(wires[i][1]);
+            nodes[wires[i][1]].add(wires[i][0]);
+        }
+
+        for (int i = 0; i < wires.length; i++) {
+            Arrays.fill(visited, false);
+            int sum = dfs(nodes, visited, 1, wires, i);
+            answer = Math.min(answer, Math.abs(2 * sum - n));
+        }
+
+        return answer;
+    }
+
+    public static int dfs(List<Integer>[] nodes, boolean[] visited, int index, int[][]wires, int block) {
+        visited[index] = true;
+        int sum = 0;
+
+        for (int node : nodes[index]) {
+            if (visited[node] || (wires[block][0] == index && wires[block][1] == node) || (wires[block][1] == index && wires[block][0] == node)) {
+                continue;
+            }
+            sum += dfs(nodes, visited, node, wires, block);
+        }
+        return sum + 1;
     }
 }

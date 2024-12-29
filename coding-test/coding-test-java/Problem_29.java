c90ef3ed8c659d29642a7def84efc2524ed23b96
Jeongmo Seo <wjdah1402@gmail.com>
2024-12-29 23:22:52 +0900

    :bulb: 문제 29 주석 수정

--- coding-test/coding-test-java/Problem_29.java
+++ coding-test/coding-test-java/Problem_29.java
@@ -2,7 +2,8 @@ import util.PrintUtil;
 
 import java.util.*;
 
-// 길 찾기 게임
+// 길 찾기 게임 (제출 완료)
+// https://school.programmers.co.kr/learn/courses/30/lessons/42892
 public class Problem_29 {
 
     public static void main(String[] args) {

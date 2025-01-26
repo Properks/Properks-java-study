85c8cbac581eaa99d8eb48660345ad05a91670f1
Jeongmo Seo <wjdah1402@gmail.com>
2025-01-26 16:25:30 +0900

    :sparkles: 문제 46 재풀이 (시간 복잡도 성공) - 대각선을 배열로 계산하는 방식

--- coding-test/coding-test-java/Problem_46.java
+++ coding-test/coding-test-java/Problem_46.java
@@ -5,10 +5,40 @@ public class Problem_46 {
         int n = 4;
         int result = solution(n);
         System.out.println(result);
+
+        int result2 = solution1(n);
+        System.out.println(result2);
     }
 
     public static int solution(int n) {
         return dfs(n, new ArrayList<>(), 0) / factorial(n);
+    }
+
+    // 백트래킹 적용, 대각선을 배열로 생각하기
+    public static int solution1(int n) {
+        int answer = 0;
+        boolean[] columns = new boolean[n];
+        boolean[] diagonal1 = new boolean[n * 2];
+        boolean[] diagonal2 = new boolean[n * 2];
+        answer = backTrack(n, columns, diagonal1, diagonal2, 0);
+        return answer;
+    }
+
+    public static int backTrack(int n, boolean[] columns, boolean[] diagonal1, boolean[] diagonal2, int count) {
+        int answer = 0;
+        if (n == count) {
+            return answer + 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!columns[i] && !diagonal1[i + count] && !diagonal2[(n - 1) + count - i]) {
+                columns[i] = diagonal1[i + count] = diagonal2[(n - 1) + count - i] = true;
+                answer += backTrack(n, columns, diagonal1, diagonal2, count + 1);
+                columns[i] = diagonal1[i + count] = diagonal2[(n - 1) + count - i] = false;
+            }
+        }
+
+        return answer;
     }
 
     // 백트래킹이 아닌 완전 탐색임... 조합인데 순열로 되어서 factorial로 나누어 주어야함...

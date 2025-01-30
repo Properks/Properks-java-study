c2a86a0933160948c67aa1d8937c73ba4a0aec45
Jeongmo Seo <wjdah1402@gmail.com>
2025-01-30 20:31:15 +0900

    :sparkles: 문제 47 다른 방식 풀이 성공 - For문 섞어서 백트래킹

--- coding-test/coding-test-java/Problem_47.java
+++ coding-test/coding-test-java/Problem_47.java
@@ -1,6 +1,8 @@
 import java.util.*;
 
 public class Problem_47 {
+    static int max = 0;
+    static int[] answer = {-1};
     public static void main(String[] args) {
         int n1 = 5;
         int[] info1 = {2,1,1,1,0,0,0,0,0,0,0};
@@ -11,13 +13,13 @@ public class Problem_47 {
         int n3 = 9;
         int[] info3 = {0,0,1,2,0,1,1,1,1,1,1};
 
-        int[] result1 = solution(n1, info1);
+        int[] result1 = solution1(n1, info1);
         System.out.println(Arrays.toString(result1));
 
-        int[] result2 = solution(n2, info2);
+        int[] result2 = solution1(n2, info2);
         System.out.println(Arrays.toString(result2));
 
-        int[] result3 = solution(n3, info3);
+        int[] result3 = solution1(n3, info3);
         System.out.println(Arrays.toString(result3));
     }
 
@@ -28,6 +30,12 @@ public class Problem_47 {
         if (Arrays.stream(answer).allMatch(value -> value == 0)) {
             return new int[]{-1};
         }
+        return answer;
+    }
+
+    public static int[] solution1(int n, int[] info) {
+        answer = new int[]{-1};
+        backTrack1(n, info, new int[11], 0);
         return answer;
     }
 
@@ -55,6 +63,22 @@ public class Problem_47 {
         return leftScore > rightScore ? left : right;
     }
 
+    // TODO: for문 섞어서 해보자 2개의 분기 이외의 상황
+    public static void backTrack1(int n, int[] info, int[] pick, int index) {
+        if (n <= 0 || index >= 11) {
+            calculateScore1(info, pick);
+            return;
+        }
+
+
+        for (int i = index; i < 11; i++ ) {
+            int count = Math.min(n, info[i] + 1);
+            pick[i] = count;
+            backTrack1(n - count, info, pick, i + 1);
+            pick[i] = 0;
+        }
+    }
+
     public static int calculateScore(int[] info, int[] pick) {
         int peachScore = 0;
         int ryanScore = 0;
@@ -67,5 +91,24 @@ public class Problem_47 {
             }
         }
         return ryanScore > peachScore ? ryanScore : -1;
+    }
+
+    public static void calculateScore1(int[] info, int[] pick) {
+        int peachScore = 0;
+        int ryanScore = 0;
+        for (int i = 0; i < 11; i++) {
+            if (info[i] < pick[i]) {
+                ryanScore += (10 - i);
+            }
+            else if (info[i] > pick[i] || (info[i] != 0 && info[i] == pick[i])) {
+                peachScore += (10 - i);
+            }
+        }
+        if (ryanScore > peachScore) {
+            if (ryanScore > max) {
+                answer = pick.clone();
+                max = ryanScore;
+            }
+        }
     }
 }

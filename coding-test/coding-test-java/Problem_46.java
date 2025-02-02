5624aa225f47e584f8a2c33479d2a72569cfd1bd
Jeongmo Seo <wjdah1402@gmail.com>
2025-02-02 16:58:16 +0900

    :sparkles: 문제 47 다른 방식 풀이 성공 (함수에서의 성능 차이) - 비슷하게 구현했지만 isValid 함수에서 성능 차이

--- coding-test/coding-test-java/Problem_46.java
+++ coding-test/coding-test-java/Problem_46.java
@@ -3,15 +3,21 @@ import java.util.*;
 public class Problem_46 {
     public static void main(String[] args) {
         int n = 4;
+        long start = System.nanoTime();
         int result = solution(n);
         System.out.println(result);
+        // 시간 복잡도는 O(n!)이지만 백트래킹 여부를 판단하는 isValid에서 성능차이가 난다. (List에 넣고 빼는 것도 성능차이가 날 것으로 예상)
+        System.out.println("Result1: " + (System.nanoTime() - start));
 
+        start = System.nanoTime();
         int result2 = solution1(n);
         System.out.println(result2);
+        System.out.println("Result2: " + (System.nanoTime() - start));
+        // 10배 가량 차이
     }
 
     public static int solution(int n) {
-        return dfs(n, new ArrayList<>(), 0) / factorial(n);
+        return dfs(n, new ArrayList<>(), 0);
     }
 
     // 백트래킹 적용, 대각선을 배열로 생각하기
@@ -42,6 +48,7 @@ public class Problem_46 {
     }
 
     // 백트래킹이 아닌 완전 탐색임... 조합인데 순열로 되어서 factorial로 나누어 주어야함...
+    // 답과 유사하게 구현 시간 복잡도: O(n!)
     public static int dfs(int n, List<Block> blocks, int count) {
         int answer = 0;
 
@@ -50,12 +57,11 @@ public class Problem_46 {
         }
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (isValid(blocks, new Block(i, j))) {
-                    List<Block> clone = new ArrayList<>(blocks);
-                    clone.add(new Block(i, j));
-                    answer += dfs(n, clone, count + 1);
-                }
+            Block newBlock = new Block(i, count);
+            if (isValid(blocks, newBlock)) {
+                blocks.add(newBlock);
+                answer += dfs(n, blocks, count + 1);
+                blocks.remove(newBlock);
             }
         }
         return answer;
@@ -66,11 +72,7 @@ public class Problem_46 {
             int x = block.x - check.x;
             int y = block.y - check.y;
             // 직선
-            if (x == 0 || y == 0) {
-                return false;
-            }
-            // 대각선
-            if (Math.abs(x) - Math.abs(y) == 0) {
+            if (x == 0 || y == 0 || Math.abs(x) - Math.abs(y) == 0) {
                 return false;
             }
         }
@@ -85,11 +87,4 @@ public class Problem_46 {
         }
     }
 
-    public static int factorial(int n) {
-        if (n == 1) {
-            return 1;
-        }
-
-        return n * factorial(n - 1);
-    }
 }
